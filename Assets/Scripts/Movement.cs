7a4fab959cf579633e72c329902eadeeb2e15431
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-01-21 19:24:15 +0000

    FIX THIS SHIT

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
     // Use this for initialization
     public float speed;
@@ -11,7 +12,7 @@ public class Movement : MonoBehaviour {
 
     private SoundManager thisSoundManager;
     private Rigidbody2D thisRigidBody;
-            
+
     void Start()
     {
         thisRigidBody = GetComponent<Rigidbody2D>();
@@ -21,35 +22,18 @@ public class Movement : MonoBehaviour {
     void Update()
     {
         freq = thisSoundManager.Frequencia;
-        if(freq > threshold)
+        if (freq > threshold)
         {
             Vector2 v = Vector2.up * speed * Time.deltaTime;
             thisRigidBody.AddForce(v);
-        }
-        
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            Vector2 v = new Vector2(0, speed);
-            thisRigidBody.AddForce(v * speed);
-        }
-        else if( Input.GetKey(KeyCode.DownArrow))
-        {
-            Vector2 v = new Vector2(0, -speed);
-            thisRigidBody.AddForce(v * speed);
-        }
-        else
-        {
-            //thisRigidBody.Sleep();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals( "Tree"))
+        if (collision.gameObject.tag.Equals("Tree"))
         {
-
-            if(this.GetComponent<Rigidbody2D>())
-                DestroyObject(this, 0);
+            DestroyObject(this.gameObject, 0);
             Debug.Log("You Lost");
         }
     }
