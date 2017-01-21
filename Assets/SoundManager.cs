216fd77223fec16b10d1dbcc77dcedd88cbd55cb
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-01-21 13:15:13 +0000

    Merging

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -5,32 +5,45 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public float testeFloat;
+    public bool done = true;
+    float[] data;
+
+    private AudioSource a;
+
+    private void Start()
+    {
+        data = new float[128];
+        a = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E))
         {
-            StartCoroutine(Record());            
-        }            
+            StartCoroutine(Record());
+        }
+        testeFloat = GetFundamentalFrequency(a);
     }
     IEnumerator Record()
     {
-        AudioClip lastRecorded;
-        lastRecorded = Microphone.Start("", false, 1, 44100);
-        AudioSource a = GetComponent<AudioSource>();
+        done = false;
+        AudioClip lastRecorded = Microphone.Start("", false, 2, 44100);
+        yield return new WaitForSecondsRealtime(2f);
         a.clip = lastRecorded;
-        yield return new WaitForSecondsRealtime(1f);
+        a.Play();
         testeFloat = GetFundamentalFrequency(a);
+
+
+        done = true;
     }
 
     float GetFundamentalFrequency(AudioSource a)
     {
         float fundamentalFrequency = 0.0f;
-        float[] data = new float[8192];
         a.GetSpectrumData(data, 0, FFTWindow.BlackmanHarris);
         float s = 0.0f;
         int i = 0;
-        for (int j = 1; j < 8192; j++)
+        for (int j = 1; j < 128; j++)
         {
             if (s < data[j])
             {
@@ -38,7 +51,7 @@ public class SoundManager : MonoBehaviour
                 i = j;
             }
         }
-        fundamentalFrequency = i * 44100 / 8192;
+        fundamentalFrequency = i * 44100 / 128;
         return fundamentalFrequency;
     }
 }
--- Assets/teste.cs
+++ Assets/teste.cs
@@ -3,59 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(AudioSource))]
-public class MicrophoneInput : MonoBehaviour
+public class teste : MonoBehaviour
 {
-    public float sensitivity = 100.0f;
-    public float loudness = 0.0f;
-    public float frequency = 0.0f;
-    public int samplerate = 11024;
-    public AudioSource audio;
+    private int amountfreq = 120;
+    private float[] freq = new float[1024];
+    private float allfreqvalue;
+    public AudioSource a;
+    public int frequencia;
 
-    void Start()
+    private void Start()
     {
-        audio = GetComponent<AudioSource>();
-        audio.clip = Microphone.Start(null, true, 10, samplerate);
-        audio.loop = true; // Set the AudioClip to loop
-        audio.mute = true; // Mute the sound, we don't want the player to hear it
-        //while (!(Microphone.GetPosition(AudioInputDevice) > 0)) { } // Wait until the recording has started
-        audio.Play(); // Play the audio source!
+        a = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        frequencia = GetMaxFreq();
     }
 
-    void Update()
+    int GetMaxFreq()
     {
-        loudness = GetAveragedVolume() * sensitivity;
-        frequency = GetFundamentalFrequency();
-    }
+        for (int s = 0; s < amountfreq; s++)
+            freq[s] = 0;
 
-    float GetAveragedVolume()
-    {
-        float[] data = new float[256];
-        float a = 0;
-        audio.GetOutputData(data, 0);
-        foreach (float s in data)
-        {
-            a += Mathf.Abs(s);
-        }
-        return a / 256;
-    }
-
-    float GetFundamentalFrequency()
-    {
-        float fundamentalFrequency = 0.0f;
-        float[] data = new float[8192];
-        audio.GetSpectrumData(data, 0, FFTWindow.BlackmanHarris);
-        float s = 0.0f;
-        int i = 0;
-        for (int j = 1; j < 8192; j++)
-        {
-            if (s < data[j])
-            {
-                s = data[j];
-                i = j;
-            }
-        }
-        fundamentalFrequency = i * samplerate / 8192;
-        return fundamentalFrequency;
+        allfreqvalue = 0;
+        int x = 0;
+        a.GetSpectrumData(freq, 0, FFTWindow.BlackmanHarris);
+        for (int s = 0; s < amountfreq; s++)
+            allfreqvalue = allfreqvalue + freq[s];
+        return x;
     }
 }
