b933451f92a9480550dc4074676095266ea22faa
Unknown <washinima99@gmail.com>
2017-01-22 15:19:57 +0000

    Au

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -14,11 +14,34 @@ public class Scroll : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        var back2_1 = Instantiate(back2);
+        var back2_2 = Instantiate(back2);
 
+        var back3_2 = Instantiate(back2);
+        var back3_1 = Instantiate(back2);
+
+        var back4_1 = Instantiate(back2);
+        var back4_2 = Instantiate(back2);
+
+
+        back2.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+        back2_1.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+        back2_2.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+
+        
+        back3.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+        back3_1.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+        back3_2.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+
+
+        back4.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+        back4_2.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
+        back4_1.GetComponent<Rigidbody2D>().AddForce(Vector2.left * b2_speed);
     }
 
     // Update is called once per frame
-    void Update () {
-        
+    void Update ()
+    {
+
     }
 }
